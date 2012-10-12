2dcca0d84ff75c3001584551cd4f7d555341f730
lancethepants <lancethepants@gmail.com>
2012-10-12 14:51:39 -0600

    Added $SRC variable AND renamed $Patches

--- install.sh
+++ install.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 
-export PATH=$PATH:`pwd`/toolchains/brcm_K26/hndtools-mipsel-linux/bin:`pwd`/toolchains/brcm_K26/hndtools-mipsel-uclibc/bin
+BASE=`pwd`
 
-Patches=`pwd`/patches
+export PATH=$PATH:$BASE/toolchains/brcm_K26/hndtools-mipsel-linux/bin:$BASE/toolchains/brcm_K26/hndtools-mipsel-uclibc/bin
+
+SRC=$BASE/src
 WGET="wget --prefer-family=IPv4"
+PATCHES=$BASE/patches
 RPATH=/opt/lib
-DEST=`pwd`/tomato
+DEST=$BASE/tomato
 LDFLAGS="-L$DEST/lib -s -Wl,--gc-sections -Wl,-rpath,$RPATH -Wl,-rpath-link=$RPATH"
 CPPFLAGS="-I$DEST/include -I$DEST/include/ncurses"
 CFLAGS="-DBCMWPA2 -funit-at-a-time -Wno-pointer-sign -mtune=mips32 -mips32"
 CONFIGURE="./configure --prefix=/opt --host=mipsel-linux"
 MAKE="make"
 
-mkdir -p src && cd src
+mkdir -p $SRC
 
 ########## ##################################################################
 # LIBFFI # ##################################################################
 ########## ##################################################################
 
+cd $SRC
 mkdir libffi && cd libffi
 $WGET ftp://sourceware.org/pub/libffi/libffi-3.0.11.tar.gz
 tar zxvf libffi-3.0.11.tar.gz
@@ -39,12 +43,12 @@ $CONFIGURE \
 
 $MAKE
 make install prefix=$DEST
-cd ../..
 
 ########## ##################################################################
 # SQLITE # ##################################################################
 ########## ##################################################################
 
+cd $SRC
 mkdir sqlite && cd sqlite
 $WGET http://www.sqlite.org/sqlite-autoconf-3071400.tar.gz
 tar zxvf sqlite-autoconf-3071400.tar.gz
@@ -57,29 +61,29 @@ $CONFIGURE
 
 $MAKE
 make install prefix=$DEST
-cd ../..
 
 ######### ###################################################################
 # BZIP2 # ###################################################################
 ######### ###################################################################
 
+cd $SRC
 mkdir bzip2 && cd bzip2 
 $WGET http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz
 tar zxvf bzip2-1.0.6.tar.gz
 cd bzip2-1.0.6
 
-patch < $Patches/bzip2.patch
-patch < $Patches/bzip2_so.patch
+patch < $PATCHES/bzip2.patch
+patch < $PATCHES/bzip2_so.patch
 
 $MAKE
 $MAKE -f Makefile-libbz2_so
 make install
-cd ../..
 
 ######## ####################################################################
 # ZLIB # ####################################################################
 ######## ####################################################################
 
+cd $SRC
 mkdir zlib && cd zlib
 $WGET http://zlib.net/zlib-1.2.7.tar.gz
 tar zxvf zlib-1.2.7.tar.gz
@@ -94,12 +98,12 @@ CROSS_PREFIX=mipsel-linux- \
 
 $MAKE
 make install prefix=$DEST
-cd ../..
 
 ####### #####################################################################
 # LZO # #####################################################################
 ####### #####################################################################
 
+cd $SRC
 mkdir lzo && cd lzo
 $WGET http://www.oberhumer.com/opensource/lzo/download/lzo-2.06.tar.gz
 tar zxvf lzo-2.06.tar.gz
@@ -113,21 +117,18 @@ $CONFIGURE \
 
 $MAKE
 make install prefix=$DEST
-cd ../..
 
 ########### #################################################################
 # OPENSSL # #################################################################
 ########### #################################################################
 
+cd $SRC
 mkdir openssl && cd openssl
-
 $WGET http://www.openssl.org/source/openssl-1.0.1c.tar.gz
-
 tar zxvf openssl-1.0.1c.tar.gz
-
 cd openssl-1.0.1c
 
-patch < $Patches/openssl.patch
+patch < $PATCHES/openssl.patch
 #patch -p1 < ../../patches/110-optimize-for-size.patch
 #patch -p1 < ../../patches/130-perl-path.patch
 #patch -p1 < ../../patches/140-makefile-dirs.patch
@@ -147,13 +148,13 @@ patch < $Patches/openssl.patch
 
 $MAKE CC=mipsel-linux-gcc AR="mipsel-linux-ar r" RANLIB=mipsel-linux-ranlib
 make install CC=mipsel-linux-gcc AR="mipsel-linux-ar r" RANLIB=mipsel-linux-ranlib INSTALLTOP=$DEST OPENSSLDIR=$DEST/ssl
-cd ../..
 
 
 ########### #################################################################
 # NCURSES # #################################################################
 ########### #################################################################
 
+cd $SRC
 mkdir ncurses && cd ncurses
 $WGET http://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.9.tar.gz
 tar zxvf ncurses-5.9.tar.gz
@@ -169,18 +170,18 @@ $CONFIGURE \
 
 $MAKE
 make install prefix=$DEST
-cd ../..
 
 ############### #############################################################
 # LIBREADLINE # #############################################################
 ############### #############################################################
 
+cd $SRC
 mkdir libreadline && cd libreadline
 $WGET ftp://ftp.gnu.org/gnu/readline/readline-6.2.tar.gz
 tar zxvf readline-6.2.tar.gz
 cd readline-6.2
 
-patch < $Patches/readline.patch
+patch < $PATCHES/readline.patch
 
 LDFLAGS=$LDFLAGS \
 CPPFLAGS=$CPPFLAGS \
@@ -189,12 +190,12 @@ $CONFIGURE
 
 $MAKE
 make install prefix=$DEST
-cd ../..
 
 ########### #################################################################
 # LIBGDBM # #################################################################
 ########### #################################################################
 
+cd $SRC
 mkdir libgdbm && cd libgdbm
 $WGET ftp://ftp.gnu.org/gnu/gdbm/gdbm-1.10.tar.gz
 tar zxvf gdbm-1.10.tar.gz
@@ -207,12 +208,12 @@ $CONFIGURE
 
 $MAKE
 make install prefix=$DEST
-cd ../..
 
 ####### #####################################################################
 # tcl # #####################################################################
 ####### #####################################################################
 
+cd $SRC
 mkdir tcl && cd tcl
 $WGET http://downloads.sourceforge.net/project/tcl/Tcl/8.5.12/tcl8.5.12-src.tar.gz
 tar zxvf tcl8.5.12-src.tar.gz
@@ -228,16 +229,16 @@ $CONFIGURE \
 ac_cv_func_strtod=yes \
 tcl_cv_strtod_buggy=1
 
-patch < $Patches/tcl.patch
+patch < $PATCHES/tcl.patch
 
 $MAKE
 make install prefix=$DEST exec_prefix=$DEST libdir=$DEST/lib includedir=$DEST/include
-cd ../../..
 
 ######### ###################################################################
 # bsdbm # ###################################################################
 ######### ###################################################################
 
+cd $SRC
 mkdir bsdbm && cd bsdbm
 $WGET http://download.oracle.com/berkeley-db/db-4.7.25.tar.gz
 tar zxvf db-4.7.25.tar.gz
@@ -252,12 +253,12 @@ CFLAGS=$CFLAGS \
 
 $MAKE
 make install prefix=$DEST
-cd ../../..
 
 ########## ##################################################################
 # PYTHON # ##################################################################
 ########## ##################################################################
 
+cd $SRC
 mkdir python && cd python
 $WGET http://python.org/ftp/python/2.7.3/Python-2.7.3.tgz
 tar zxvf Python-2.7.3.tgz
@@ -282,31 +283,31 @@ cp ../Python-2.7.3-native/Parser/pgen Parser/hostpgen
 
 $MAKE HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen CROSS_COMPILE=mipsel-linux- CROSS_COMPILE_TARGET=yes HOSTARCH=mipsel-linux BUILDARCH=x86_64-linux-gnu
 make install prefix=$DEST HOSTPYTHON=../Python-2.7.3-native/python CROSS_COMPILE=mipsel-linux- CROSS_COMPILE_TARGET=yes
-cd ../..
 
 ############ ################################################################
 # POLARSSL # ################################################################
 ############ ################################################################
 
+cd $SRC
 mkdir polarssl && cd polarssl
 $WGET http://polarssl.org/code/releases/polarssl-1.1.4-gpl.tgz
 tar zxvf polarssl-1.1.4-gpl.tgz
 cd polarssl-1.1.4
 
-patch < $Patches/polarssl.patch
+patch < $PATCHES/polarssl.patch
 cd library
-patch < $Patches/polarssl_lib.patch
+patch < $PATCHES/polarssl_lib.patch
 cd ..
 
 $MAKE
 make install DESTDIR=$DEST
 ln -s libpolarssl.so $DEST/lib/libpolarssl.so.0
-cd ../..
 
 ########### #################################################################
 # LIBPCAP # #################################################################
 ########### #################################################################
 
+cd $SRC
 mkdir libpcap && cd libpcap
 $WGET http://www.tcpdump.org/release/libpcap-1.3.0.tar.gz
 tar zxvf libpcap-1.3.0.tar.gz
@@ -321,12 +322,12 @@ $CONFIGURE \
 
 $MAKE
 make install prefix=$DEST
-cd ../..
 
 ########### #################################################################
 # OPENVPN # #################################################################
 ########### #################################################################
 
+cd $SRC
 mkdir openvpn && cd openvpn
 $WGET http://swupdate.openvpn.org/community/releases/openvpn-2.3_beta1.tar.gz
 tar zxvf openvpn-2.3_beta1.tar.gz
@@ -341,12 +342,12 @@ $CONFIGURE \
 
 $MAKE
 make install prefix=$DEST
-cd ../..
 
 ######## ####################################################################
 # TINC # ####################################################################
 ######## ####################################################################
 
+cd $SRC
 mkdir tinc && cd tinc
 $WGET http://www.tinc-vpn.org/packages/tinc-1.0.19.tar.gz
 tar zxvf tinc-1.0.19.tar.gz
@@ -359,26 +360,25 @@ $CONFIGURE
 
 $MAKE
 make install prefix=$DEST
-cd ../..
 
 ############## ##############################################################
 # SETUPTOOLS # ##############################################################
 ############## ##############################################################
 
+cd $SRC
 mkdir setuptools && cd setuptools
 $WGET http://pypi.python.org/packages/source/s/setuptools/setuptools-0.6c11.tar.gz
 tar zxvf setuptools-0.6c11.tar.gz
-cd ..
 
 ########### #################################################################
 # CHEETAH # #################################################################
 ########### #################################################################
 
-cd python/Python-2.7.3/build/
+cd $SRC/python/Python-2.7.3/build/
 mv lib.linux-x86_64-2.7/ lib.linux-mipsel-2.7/
 cp -R ../../Python-2.7.3-native/build/lib.linux-x86_64-2.7/ .
-cd ../../..
 
+cd $SRC
 mkdir cheetah && cd cheetah
 $WGET http://pypi.python.org/packages/source/C/Cheetah/Cheetah-2.4.4.tar.gz
 tar zxvf Cheetah-2.4.4.tar.gz
@@ -388,49 +388,46 @@ mv dist/Cheetah-2.4.4-py2.7-linux-x86_64.egg dist/Cheetah-2.4.4-py2.7.egg
 
 mkdir -p $DEST/python_modules
 cp dist/Cheetah-2.4.4-py2.7.egg $DEST/python_modules
-cd ../..
 
-cd python/Python-2.7.3/build/
+cd $SRC/python/Python-2.7.3/build/
 rm -rf lib.linux-x86_64-2.7/
 mv lib.linux-mipsel-2.7/ lib.linux-x86_64-2.7/
-cd ../../..
 
 ######## ####################################################################
 # YENC # ####################################################################
 ######## ####################################################################
 
-cd python/Python-2.7.3/build/
+cd $SRC/python/Python-2.7.3/build/
 mv lib.linux-x86_64-2.7/ lib.linux-mipsel-2.7/
 cp -R ../../Python-2.7.3-native/build/lib.linux-x86_64-2.7/ .
-cd ../../..
 
+cd $SRC
 mkdir yenc && cd yenc
 $WGET http://www.golug.it/pub/yenc/yenc-0.4.0.tar.gz
 tar zxvf yenc-0.4.0.tar.gz
 cd yenc-0.4.0
 
-patch < $Patches/yenc.patch
+patch < $PATCHES/yenc.patch
 PYTHONPATH=../../python/Python-2.7.3/Lib/:../../setuptools/setuptools-0.6c11 ../../python/Python-2.7.3/hostpython ./setup.py bdist_egg
 mv dist/yenc-0.4.0-py2.7-linux-x86_64.egg dist/yenc-0.4.0-py2.7.egg
 mkdir -p $DEST/python_modules
 cp dist/yenc-0.4.0-py2.7.egg $DEST/python_modules
-cd ../..
 
-cd python/Python-2.7.3/build/
+cd $SRC/python/Python-2.7.3/build/
 rm -rf lib.linux-x86_64-2.7/
 mv lib.linux-mipsel-2.7/ lib.linux-x86_64-2.7/
-cd ../../..
 
 ############### #############################################################
 # PAR2CMDLINE # #############################################################
 ############### #############################################################
 
+cd $SRC
 mkdir par2cmdline && cd par2cmdline
 $WGET http://downloads.sourceforge.net/project/parchive/par2cmdline/0.4/par2cmdline-0.4.tar.gz
 tar zxvf par2cmdline-0.4.tar.gz
 cd par2cmdline-0.4
 
-patch reedsolomon.cpp $Patches/reedsolomon.cpp.patch
+patch reedsolomon.cpp $PATCHES/reedsolomon.cpp.patch
 
 LDFLAGS=$LDFLAGS \
 CPPFLAGS=$CPPFLAGS \
@@ -440,33 +437,32 @@ $CONFIGURE
 make clean 
 $MAKE
 make install prefix=$DEST
-cd ../..
 
 ######### ###################################################################
 # UNRAR # ###################################################################
 ######### ###################################################################
 
+cd $SRC
 mkdir unrar && cd unrar
 $WGET http://www.rarlab.com/rar/unrarsrc-4.2.4.tar.gz
 tar zxvf unrarsrc-4.2.4.tar.gz
 cd unrar
 
 mv makefile.unix Makefile
-patch < $Patches/unrar.patch
+patch < $PATCHES/unrar.patch
 
 $MAKE CXX=mipsel-linux-g++ CXXFLAGS=$CPPFLAGS STRIP=mipsel-linux-strip
 make install DESTDIR=$DEST
-cd ../..
 
 ############# ###############################################################
 # pyOpenSSL # ###############################################################
 ############# ###############################################################
 
-cd python/Python-2.7.3/build/
+cd $SRC/python/Python-2.7.3/build/
 mv lib.linux-x86_64-2.7/ lib.linux-mipsel-2.7/
 cp -R ../../Python-2.7.3-native/build/lib.linux-x86_64-2.7/ .
-cd ../../..
 
+cd $SRC
 mkdir pyopenssl && cd pyopenssl
 $WGET http://pypi.python.org/packages/source/p/pyOpenSSL/pyOpenSSL-0.13.tar.gz
 tar zxvf pyOpenSSL-0.13.tar.gz
@@ -479,23 +475,22 @@ mv dist/pyOpenSSL-0.13-py2.7-linux-x86_64.egg dist/pyOpenSSL-0.13-py2.7.egg
 
 mkdir -p $DEST/python_modules
 cp dist/pyOpenSSL-0.13-py2.7.egg $DEST/python_modules
-cd ../..
 
-cd python/Python-2.7.3/build/
+cd $SRC/python/Python-2.7.3/build/
 rm -rf lib.linux-x86_64-2.7/
 mv lib.linux-mipsel-2.7/ lib.linux-x86_64-2.7/
-cd ../../..
 
 ######### ###################################################################
 # UCARP # ###################################################################
 ######### ###################################################################
 
+cd $SRC
 mkdir ucarp && cd ucarp
 $WGET http://download.pureftpd.org/pub/ucarp/ucarp-1.5.2.tar.gz
 tar zxvf ucarp-1.5.2.tar.gz
 cd ucarp-1.5.2
 
-patch < $Patches/ucarp.patch
+patch < $PATCHES/ucarp.patch
 
 LDFLAGS=$LDFLAGS \
 CPPFLAGS=$CPPFLAGS \
@@ -504,12 +499,12 @@ $CONFIGURE
 
 $MAKE
 make install prefix=$DEST
-cd ../..
 
 ############ ################################################################
 # DNSCRYPT # ################################################################
 ############ ################################################################
 
+cd $SRC
 mkdir dnscrypt && cd dnscrypt
 $WGET https://github.com/downloads/opendns/dnscrypt-proxy/dnscrypt-proxy-1.1.0.tar.gz
 tar zxvf dnscrypt-proxy-1.1.0.tar.gz
@@ -523,4 +518,3 @@ $CONFIGURE \
 
 $MAKE
 make install prefix=$DEST
-cd ../..
