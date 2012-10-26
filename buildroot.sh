f85d16012e082a8da911f47be365b74fca65be27
lancethepants <lancethepants@gmail.com>
2012-10-26 08:42:54 -0600

    Native router compilation toolset, work in progress.

---
+++ buildroot.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+
+BASE=`pwd`
+SRC=$BASE/src
+WGET="wget --prefer-family=IPv4"
+PATCHES=$BASE/patches
+RPATH=/opt/lib
+DEST=/opt
+LDFLAGS="-L$DEST/lib -s -Wl,--dynamic-linker=/opt/lib/ld-uClibc.so.0 -Wl,--gc-sections -Wl,-rpath,$RPATH -Wl,-rpath-link=$RPATH"
+CPPFLAGS="-I$DEST/include -I$DEST/include/ncurses"
+CFLAGS="-DBCMWPA2 -funit-at-a-time -Wno-pointer-sign -mtune=mips32 -mips32"
+CONFIGURE="./configure --prefix=/opt --host=mipsel-linux"
+MAKE="make"
+
+mkdir -p $SRC
+
+$WGET http://wl500g-repo.googlecode.com/files/entware-toolchain-r4667-amd64.tgz
+tar zxvf entware-toolchain-r4667-amd64.tgz
+mv opt/entware-toolchain/ ./toolchain
+rm -rf opt/ entware-toolchain-r4667-amd64.tgz
+export PATH=$PATH:$BASE/toolchain/bin:$BASE/toolchain/mipsel-linux/bin
+
+cd $DEST
+sudo rm -rf bin/ docs/ include/ lib/ libexec/ man/ sbin/ share/ mipsel-linux/
+sudo mkdir bin docs include lib libexec man sbin share mipsel-linux
+sudo chown lance:lance bin/ docs/ include/ lib/ libexec/ man/ sbin/ share/ mipsel-linux/
+
+
+############ ################################################################
+# BINUTILS # ################################################################
+############ ################################################################
+
+cd $SRC
+mkdir binutils && cd binutils
+$WGET http://ftp.gnu.org/gnu/binutils/binutils-2.22.tar.gz
+tar zxvf binutils-2.22.tar.gz
+mkdir build-binutils && cd build-binutils
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+../binutils-2.22/$CONFIGURE \
+--disable-werror
+
+$MAKE
+make install
+
+####### #####################################################################
+# GMP # #####################################################################
+####### #####################################################################
+
+cd $SRC
+mkdir gmp && cd gmp
+$WGET ftp://ftp.gmplib.org/pub/gmp-5.0.5/gmp-5.0.5.tar.bz2
+tar xvjf gmp-5.0.5.tar.bz2
+cd gmp-5.0.5
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+$CONFIGURE \
+--enable-cxx 
+
+$MAKE
+make install
+
+######## ####################################################################
+# MPFR # ####################################################################
+######## ####################################################################
+
+cd $SRC
+mkdir mpfr && cd mpfr
+$WGET http://www.mpfr.org/mpfr-current/mpfr-3.1.1.tar.gz
+tar zxvf mpfr-3.1.1.tar.gz
+cd mpfr-3.1.1
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+$CONFIGURE
+
+$MAKE
+make install
+
+####### #####################################################################
+# MPC # #####################################################################
+####### #####################################################################
+
+cd $SRC
+mkdir mpc && cd mpc
+$WGET http://www.multiprecision.org/mpc/download/mpc-1.0.1.tar.gz
+tar zxvf mpc-1.0.1.tar.gz
+cd mpc-1.0.1
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+$CONFIGURE
+
+$MAKE
+make install
+
+####### #####################################################################
+# GCC # #####################################################################
+####### #####################################################################
+
+cd $SRC
+mkdir gcc && cd gcc
+$WGET http://www.netgull.com/gcc/releases/gcc-4.6.3/gcc-4.6.3.tar.gz
+tar zxvf gcc-4.6.3.tar.gz
+
+mkdir gcc-build && cd gcc-build
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+../gcc-4.6.3/$CONFIGURE --target=mipsel-linux \
+--with-mpc-include=$DEST/include \
+--with-mpc-lib=$DEST/lib \
+--with-mpfr-include=$DEST/include \
+--with-mpfr-lib=$DEST/lib \
+--with-gmp-include=$DEST/include \
+--with-gmp-lib=$DEST/lib \
+--enable-version-specific-runtime-libs \
+--enable-languages=c,c++ \
+--with-gnu-as --with-gnu-ld --disable-nls -enable-werror=no --disable-libstdcxx-pch
+
+make
+make install
