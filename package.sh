e49df634c11a60a94d4467f4985db718ffa4555a
lancethepants <lancethepants@gmail.com>
2012-11-12 12:09:46 -0700

    Add script to install python modules after it has been transferred to the device.

--- package.sh
+++ package.sh
@@ -46,6 +46,51 @@ ln -sf libutil-0.9.32.so $DEST_LIB/libutil.so.0
 cp -f $SOURCE_LIB/libgcc_s.so.1 $DEST_LIB
 ln -sf libgcc_s.so.1 $DEST_LIB/libgcc_s.so
 
+cd $BASE/tomato/python_modules
+
+if [ ! -d setuptools ]
+then
+
+mkdir setuptools && cd setuptools
+wget http://pypi.python.org/packages/source/s/setuptools/setuptools-0.6c11.tar.gz
+
+fi
+
+cd $BASE/tomato/python_modules
+
+if [ ! -d markdown ]
+then
+
+mkdir markdown && cd markdown
+wget http://pypi.python.org/packages/source/M/Markdown/Markdown-2.2.1.tar.gz
+
+fi
+
+cd $BASE/tomato/python_modules
+
+echo "#!/bin/sh" > install_modules.sh
+
+echo "cd /opt/python_modules/setuptools" >> install_modules.sh
+echo "rm -rf setuptools-0.6c11" >> install_modules.sh
+echo "tar zxvf setuptools-0.6c11.tar.gz" >> install_modules.sh
+echo "cd setuptools-0.6c11/" >> install_modules.sh
+echo "./setup.py build" >> install_modules.sh
+echo "./setup.py install" >> install_modules.sh
+
+echo "cd /opt/python_modules/markdown" >> install_modules.sh
+echo "rm -rf Markdown-2.2.1" >> install_modules.sh
+echo "tar zxvf Markdown-2.2.1.tar.gz" >> install_modules.sh
+echo "cd Markdown-2.2.1/" >> install_modules.sh
+echo "./setup.py build" >> install_modules.sh
+echo "./setup.py install" >> install_modules.sh
+
+echo "cd /opt/python_modules" >> install_modules.sh
+echo "easy_install -Z Cheetah-2.4.4-py2.7.egg" >> install_modules.sh
+echo "easy_install -Z pyOpenSSL-0.13-py2.7.egg " >> install_modules.sh
+echo "easy_install -Z yenc-0.4.0-py2.7.egg" >> install_modules.sh
+
+chmod +x install_modules.sh
+
 cd $BASE/tomato
 
 tar zvcf $BASE/opt.tgz bin/ docs/ include/ lib/ man/ python_modules/ sbin/ share/ ssl/
