0fbc284faa5ba1050aadfa9fefbfad8c9d320c65
lancethepants <lancethepants@gmail.com>
2013-01-20 17:01:46 -0700

    Add runtime script.

--- do-all.sh
+++ do-all.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+T="$(date +%s)"
+
 ./toolchain_install.sh
 ./install.sh
 ./buildroot.sh
 ./package.sh
+
+T="$(($(date +%s)-T))"
+echo "Time in seconds: ${T}"
+
+printf "%02d:%02d:%02d:%02d\n" "$((T/86400))" "$((T/3600%24))" "$((T/60%60))" "$((T%60))"
