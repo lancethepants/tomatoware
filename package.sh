fc267750f489d778fccade07e984cd3d8c0b9538
lancethepants <lancethepants@gmail.com>
2013-03-26 13:25:49 -0600

    Add /etc/profile, modified from the entware project.

--- package.sh
+++ package.sh
@@ -58,6 +58,31 @@ echo "easy_install -Z yenc-0.4.0-py2.7.egg" >> install_modules.sh
 
 chmod +x install_modules.sh
 
+cd $DEST/etc
+
+echo "#!/bin/sh" > profile
+echo "" >> profile
+echo "# Please note it's not a system-wide settings, it's only for a current" >> profile
+echo "# terminal session. Point your f\w (if necessery) to execute /opt/etc/profile" >> profile
+echo "# at console logon." >> profile
+echo "" >> profile
+echo "export PATH='/opt/usr/sbin:/opt/sbin:/opt/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin'" >> profile
+echo "export TERM=xterm" >> profile
+echo "export TMP=/opt/tmp" >> profile
+echo "export TEMP=/opt/tmp" >> profile
+echo "export PKG_CONFIG_LIBDIR=/opt/lib/pkgconfig" >> profile
+echo "export M4=/opt/bin/m4" >> profile
+echo "" >> profile
+echo "# You may define localization" >> profile
+echo "#export LANG='ru_RU.UTF-8'" >> profile
+echo "#export LC_ALL='ru_RU.UTF-8'" >> profile
+echo "" >> profile
+echo "alias ls='ls --color'" >> profile
+echo "alias o='cd /opt'" >> profile
+echo "alias uptime='/usr/bin/uptime'" >> profile
+
+chmod +x profile
+
 cd $BASE/opt
 
 tar zvcf $BASE/opt.tgz bin/ docs/ etc/ include/ lib/ libexec/ man/ python_modules/ sbin/ share/ ssl/ var/
