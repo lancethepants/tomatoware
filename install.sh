88d1c9fefacfc0098ae07ea400e97292210924eb
lancethepants <lancethepants@gmail.com>
2013-08-26 09:10:03 -0600

    Update curl to latest.

--- install.sh
+++ install.sh
@@ -220,12 +220,12 @@ fi
 cd $SRC/curl
 
 if [ ! -f .extracted ]; then
-	rm -rf curl-7.30.0
-	tar zxvf curl-7.30.0.tar.gz
+	rm -rf curl-7.32.0
+	tar zxvf curl-7.32.0.tar.gz
 	touch .extracted
 fi
 
-cd curl-7.30.0
+cd curl-7.32.0
 
 if [ ! -f .patched ]; then
 	patch < $PATCHES/curl.patch
@@ -256,7 +256,7 @@ if [ ! -f .certs_installed ]; then
 	cd $DEST/ssl/certs
 	curl http://curl.haxx.se/ca/cacert.pem | awk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "cert" n ".pem"}'
 	c_rehash .
-	touch $SRC/curl/curl-7.30.0/.certs_installed
+	touch $SRC/curl/curl-7.32.0/.certs_installed
 fi
 
 ######### ###################################################################
