9aae32259a6fb39ec7847f3a99520b5a05140d20
lancethepants <lancethepants@gmail.com>
2013-08-26 14:09:23 -0600

    Update git to latest usable source. 1.8.3.4. 1.8.4 doesn't compile at this time.

--- install.sh
+++ install.sh
@@ -1119,12 +1119,12 @@ fi
 cd $SRC/git
 
 if [ ! -f .extracted ]; then
-	rm -rf git-1.8.3
-	tar zxvf git-1.8.3.tar.gz
+	rm -rf git-1.8.3.4
+	tar zxvf git-1.8.3.4.tar.gz
 	touch .extracted
 fi
 
-cd git-1.8.3
+cd git-1.8.3.4
 
 if [ ! -f .built ]; then
 	make distclean
@@ -1163,7 +1163,7 @@ if [ ! -f .installed ]; then
 	install DESTDIR=$BASE
 	touch .installed
 fi
-
+exit 1;
 
 ########## ##################################################################
 # STRACE # ##################################################################
