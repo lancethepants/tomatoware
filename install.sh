e046a771f166895e09e31399a585d78c53f7bf9a
lancethepants <lancethepants@gmail.com>
2013-09-16 12:57:37 -0600

    Update SQLite to latest. 3080002.

--- install.sh
+++ install.sh
@@ -604,12 +604,12 @@ fi
 cd $SRC/sqlite
 
 if [ ! -f .extracted ]; then
-	rm -rf sqlite-autoconf-3080000
-	tar zxvf sqlite-autoconf-3080000.tar.gz
+	rm -rf sqlite-autoconf-3080002
+	tar zxvf sqlite-autoconf-3080002.tar.gz
 	touch .extracted
 fi
 
-cd sqlite-autoconf-3080000
+cd sqlite-autoconf-3080002
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
