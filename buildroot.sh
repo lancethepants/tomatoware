5bd75d4d161b37a77b11d19f249722d2ff5f08b2
lancethepants <lancethepants@gmail.com>
2013-09-16 15:26:52 -0600

    Update Automake to latest. 1.14

--- buildroot.sh
+++ buildroot.sh
@@ -351,12 +351,12 @@ fi
 cd $SRC/automake
 
 if [ ! -f .extracted ]; then
-	rm -rf automake-1.13.3
-	tar zxvf automake-1.13.3.tar.gz
+	rm -rf automake-1.14
+	tar zxvf automake-1.14.tar.gz
 	touch .extracted
 fi
 
-cd automake-1.13.3
+cd automake-1.14
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
