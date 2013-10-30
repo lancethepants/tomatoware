bb85436a072803488a1d464413a0e46cf2155fa6
lancethepants <lancethepants@gmail.com>
2013-10-30 08:16:38 -0600

    Update Asterisk to latest. 1.6.0

--- scripts/asterisk.sh
+++ scripts/asterisk.sh
@@ -148,12 +148,12 @@ fi
 cd $SRC/asterisk
 
 if [ ! -f .extracted ]; then
-	rm -rf asterisk-11.5.1
-	tar zxvf asterisk-11.5.1.tar.gz
+	rm -rf asterisk-11.6.0
+	tar zxvf asterisk-11.6.0.tar.gz
 	touch .extracted
 fi
 
-cd asterisk-11.5.1
+cd asterisk-11.6.0
 
 if [ ! -f .patched ]; then
 	sed -i 's,\/etc\/localtime,\/opt\/etc\/localtime,g' main/stdtime/localtime.c
