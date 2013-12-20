9034a04c76029e5d7129c3187b83209793b7f246
lancethepants <lancethepants@gmail.com>
2013-12-20 10:16:13 -0700

    Update libpcap to latest. 1.5.2.

--- scripts/install.sh
+++ scripts/install.sh
@@ -339,12 +339,12 @@ fi
 cd $SRC/libpcap
 
 if [ ! -f .extracted ]; then
-	rm -rf libpcap-1.5.1
-	tar zxvf libpcap-1.5.1.tar.gz
+	rm -rf libpcap-1.5.2
+	tar zxvf libpcap-1.5.2.tar.gz
 	touch .extracted
 fi
 
-cd libpcap-1.5.1
+cd libpcap-1.5.2
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
