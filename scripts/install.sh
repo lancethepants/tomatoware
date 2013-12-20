2fa23089651c82abf3a027a9c39ac9f8cc083950
lancethepants <lancethepants@gmail.com>
2013-12-20 11:06:41 -0700

    Update git to latest. 1.8.5.2.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1180,12 +1180,12 @@ fi
 cd $SRC/git
 
 if [ ! -f .extracted ]; then
-	rm -rf git-1.8.4.3
-	tar zxvf git-1.8.4.3.tar.gz
+	rm -rf git-1.8.5.2
+	tar zxvf git-1.8.5.2.tar.gz
 	touch .extracted
 fi
 
-cd git-1.8.4.3
+cd git-1.8.5.2
 
 if [ ! -f .built ]; then
 	make distclean
