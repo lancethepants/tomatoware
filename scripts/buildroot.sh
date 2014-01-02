5576f5457302e62b631318ba028118cb000bcfb2
lancethepants <lancethepants@gmail.com>
2014-01-02 07:53:11 -0700

    Update tar to latest. 2.27.1.

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -843,12 +843,12 @@ fi
 cd $SRC/tar
 
 if [ ! -f .extracted ]; then
-	rm -rf tar-1.27
-	tar zxvf tar-1.27.tar.gz
+	rm -rf tar-1.27.1
+	tar zxvf tar-1.27.1.tar.gz
 	touch .extracted
 fi
 
-cd tar-1.27
+cd tar-1.27.1
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
