148ed96f57153e3f44b382022d51870c9062e786
lancethepants <lancethepants@gmail.com>
2014-03-01 18:23:05 -0700

    Update Check to latest. 0.9.12.

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -417,12 +417,12 @@ fi
 cd $SRC/check
 
 if [ ! -f .extracted ]; then
-	rm -rf check-0.9.11
-	tar zxvf check-0.9.11.tar.gz
+	rm -rf check-0.9.12
+	tar zxvf check-0.9.12.tar.gz
 	touch .extracted
 fi
 
-cd check-0.9.11
+cd check-0.9.12
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
