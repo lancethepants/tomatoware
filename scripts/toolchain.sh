1c7572926a04c900ac0cffa98dd1f290dcecf73c
lancethepants <lancethepants@gmail.com>
2014-03-04 22:50:06 -0700

    Fix toolchain broken gcc url, and priorize it over pokey berlin ftp.

--- scripts/toolchain.sh
+++ scripts/toolchain.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 
 BASE=`pwd`
+export PATCHES=$BASE/patches
 
 if [ ! -d /opt/entware-toolchain ]
 then
@@ -12,6 +13,7 @@ then
 	cd $BASE/toolchain
 	svn co http://wl500g-repo.googlecode.com/svn/trunk/ ./rtn
 	cd ./rtn/toolchain/mipsel-hardfloat
+	patch -p1 Makefile < $PATCHES/toolchain/mipsel-hardfloat.patch
 	sed -i 's,\/opt,'"$PREFIX"',g' \
 	001-uclibc-ldso-search-path.patch \
 	002-uclibc-ldconfig-opt.patch \
