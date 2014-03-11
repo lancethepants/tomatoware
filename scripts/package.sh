7f42e7488097381faf943f14085e2fea9c0857ad
lancethepants <lancethepants@gmail.com>
2014-03-11 06:54:55 -0600

    Correct missing broken path in profile.

--- scripts/package.sh
+++ scripts/package.sh
@@ -84,9 +84,9 @@ echo "" >> profile
 
 if [ $PREFIX = "/opt" ];
 then
-	echo "export PATH='opt/usr/sbin:opt/sbin:opt/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin'" >> profile
+	echo "export PATH='/opt/usr/sbin:/opt/sbin:/opt/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin'" >> profile
 else
-	echo "export PATH='$PREFIX/sbin:$PREFIX/bin:opt/usr/sbin:opt/sbin:opt/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin'" >> profile
+	echo "export PATH='$PREFIX/sbin:$PREFIX/bin:/opt/usr/sbin:/opt/sbin:/opt/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin'" >> profile
 fi
 
 echo "export TERM=xterm" >> profile
