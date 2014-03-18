23cca440407b82daf60582add0a57fed7354a340
lancethepants <lancethepants@gmail.com>
2014-03-17 20:06:37 -0600

    Update openssh to latest. 6.6p1

--- scripts/install.sh
+++ scripts/install.sh
@@ -1314,12 +1314,12 @@ fi
 cd $SRC/openssh
 
 if [ ! -f .extracted ]; then
-	rm -rf openssh-6.5p1
-	tar zxvf openssh-6.5p1.tar.gz
+	rm -rf openssh-6.6p1
+	tar zxvf openssh-6.6p1.tar.gz
 	touch .extracted
 fi
 
-cd openssh-6.5p1
+cd openssh-6.6p1
 
 if [ ! -f .patched ]; then
 	patch -p1 < $PATCHES/openssh/openssh-fix-pam-uclibc-pthreads-clash.patch
