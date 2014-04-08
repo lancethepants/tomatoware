36350f785906538b09e7a3aa35e64fea7b99bbf6
lancethepants <lancethepants@gmail.com>
2014-04-08 09:17:44 -0600

    Update git to latest. 1.9.1.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1197,12 +1197,12 @@ fi
 cd $SRC/git
 
 if [ ! -f .extracted ]; then
-	rm -rf git-1.9.0
-	tar zxvf git-1.9.0.tar.gz
+	rm -rf git-1.9.1
+	tar zxvf git-1.9.1.tar.gz
 	touch .extracted
 fi
 
-cd git-1.9.0
+cd git-1.9.1
 
 if [ ! -f .built ]; then
 	make distclean
