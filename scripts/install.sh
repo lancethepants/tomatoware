ee266867f22d042b13b5b3ef610ee9853d97c718
lancethepants <lancethepants@gmail.com>
2014-04-22 14:16:06 -0600

    Update pcre to latest. 8.35.

--- scripts/install.sh
+++ scripts/install.sh
@@ -914,12 +914,12 @@ fi
 cd $SRC/pcre
 
 if [ ! -f .extracted ]; then
-	rm -rf pcre-8.34
-	tar zxvf pcre-8.34.tar.gz
+	rm -rf pcre-8.35
+	tar zxvf pcre-8.35.tar.gz
 	touch .extracted
 fi
 
-cd pcre-8.34
+cd pcre-8.35
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
