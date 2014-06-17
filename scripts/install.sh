301de94db635329b92710c72f121415f86c49e13
lancethepants <lancethepants@gmail.com>
2014-06-17 07:57:38 -0600

    Update gettext to latest. 0.19.1.

--- scripts/install.sh
+++ scripts/install.sh
@@ -203,12 +203,12 @@ fi
 cd $SRC/gettext
 
 if [ ! -f .extracted ]; then
-	rm -rf gettext-0.18.3.2
-	tar zxvf gettext-0.18.3.2.tar.gz
+	rm -rf gettext-0.19.1
+	tar zxvf gettext-0.19.1.tar.gz
 	touch .extracted
 fi
 
-cd gettext-0.18.3.2
+cd gettext-0.19.1
 
 if [ ! -f .patched ]; then
 	patch -p1 < $PATCHES/spawn.patch
