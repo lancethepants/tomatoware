02d7a92a4bf8a97d082e8fd61e41abaa11226d86
lancethepants <lancethepants@gmail.com>
2014-10-13 08:39:05 -0600

    Update git to latest. 2.1.2.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1196,12 +1196,12 @@ fi
 cd $SRC/git
 
 if [ ! -f .extracted ]; then
-	rm -rf git-2.1.1
-	tar zxvf git-2.1.1.tar.gz
+	rm -rf git-2.1.2
+	tar zxvf git-2.1.2.tar.gz
 	touch .extracted
 fi
 
-cd git-2.1.1
+cd git-2.1.2
 
 if [ ! -f .built ]; then
 	make distclean
