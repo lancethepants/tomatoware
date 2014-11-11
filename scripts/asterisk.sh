1a3a0e2d09c1758447d6a8c90eb1d0eeb686a59b
lancethepants <lancethepants@gmail.com>
2014-11-11 10:33:51 -0700

    Update asterisk to latest. 11.14.0.

--- scripts/asterisk.sh
+++ scripts/asterisk.sh
@@ -188,12 +188,12 @@ fi
 cd $SRC/asterisk
 
 if [ ! -f .extracted ]; then
-	rm -rf asterisk-11.13.1
-	tar zxvf asterisk-11.13.1.tar.gz
+	rm -rf asterisk-11.14.0
+	tar zxvf asterisk-11.14.0.tar.gz
 	touch .extracted
 fi
 
-cd asterisk-11.13.1
+cd asterisk-11.14.0
 
 if [ ! -f .patched ]; then
 	sed -i 's,\/etc\/localtime,'"$PREFIX"'\/etc\/localtime,g' main/stdtime/localtime.c
