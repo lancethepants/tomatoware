ab6e808fb00cf9ed1d7f7f8a6c7b86bb455a2286
lancethepants <lancethepants@gmail.com>
2014-11-21 21:43:02 -0700

    Update tcl to latest. 8.6.3.

--- scripts/install.sh
+++ scripts/install.sh
@@ -581,12 +581,12 @@ fi
 cd $SRC/tcl
 
 if [ ! -f .extracted ]; then
-	rm -rf cd tcl8.6.2/unix
-	tar zxvf tcl8.6.2-src.tar.gz
+	rm -rf cd tcl8.6.3/unix
+	tar zxvf tcl8.6.3-src.tar.gz
 	touch .extracted
 fi
 
-cd tcl8.6.2/unix
+cd tcl8.6.3/unix
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
