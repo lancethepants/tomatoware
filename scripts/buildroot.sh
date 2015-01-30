71f52df63a6e1e9a0b8c34a6d2c6e7069ac5c859
lancethepants <lancethepants@gmail.com>
2015-01-29 22:14:31 -0700

    Update bison to latest. 3.0.4.

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -413,12 +413,12 @@ fi
 cd $SRC/bison
 
 if [ ! -f .extracted ]; then
-	rm -rf bison-3.0.2
-	tar zxvf bison-3.0.2.tar.gz
+	rm -rf bison-3.0.4
+	tar zxvf bison-3.0.4.tar.gz
 	touch .extracted
 fi
 
-cd bison-3.0.2
+cd bison-3.0.4
 
 if [ ! -f .patched ]; then
 	cp -v Makefile.in{,.orig}
