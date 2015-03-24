9bcd7fb6b6522a9bcc0e93f6dc3436bd4aa01384
lancethepants <lancethepants@gmail.com>
2015-03-24 12:57:52 -0600

    Update patch to latest. 2.7.5.

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -884,12 +884,12 @@ fi
 cd $SRC/patch
 
 if [ ! -f .extracted ]; then
-	rm -rf  patch-2.7.4
-	tar zxvf patch-2.7.4.tar.gz
+	rm -rf  patch-2.7.5
+	tar zxvf patch-2.7.5.tar.gz
 	touch .extracted
 fi
 
-cd patch-2.7.4
+cd patch-2.7.5
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
