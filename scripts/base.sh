a24ed72595998a40be06d0f3527c88d85e939068
lancethepants <lancethepants@gmail.com>
2015-03-24 11:46:57 -0600

    Update curl to latest. 7.41.0.

--- scripts/base.sh
+++ scripts/base.sh
@@ -316,12 +316,12 @@ fi
 cd $SRC/curl
 
 if [ ! -f .extracted ]; then
-	rm -rf curl-7.40.0
-	tar zxvf curl-7.40.0.tar.gz
+	rm -rf curl-7.41.0
+	tar zxvf curl-7.41.0.tar.gz
 	touch .extracted
 fi
 
-cd curl-7.40.0
+cd curl-7.41.0
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
@@ -348,7 +348,7 @@ if [ ! -f .certs_installed ]; then
 	cd $DEST/ssl/certs
 	curl http://curl.haxx.se/ca/cacert.pem | awk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "cert" n ".pem"}'
 	c_rehash .
-	touch $SRC/curl/curl-7.40.0/.certs_installed
+	touch $SRC/curl/curl-7.41.0/.certs_installed
 fi
 
 ######### ###################################################################
