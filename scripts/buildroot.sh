49a105aeb5d9a81f42b290217803cc1fdcef0d77
lancethepants <lancethepants@gmail.com>
2015-03-24 12:55:38 -0600

    Update libtool to latest. 2.4.6.

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -698,12 +698,12 @@ fi
 cd $SRC/libtool
 
 if [ ! -f .extracted ]; then
-	rm -rf libtool-2.4.5
-	tar zxvf libtool-2.4.5.tar.gz
+	rm -rf libtool-2.4.6
+	tar zxvf libtool-2.4.6.tar.gz
 	touch .extracted
 fi
 
-cd libtool-2.4.5
+cd libtool-2.4.6
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
