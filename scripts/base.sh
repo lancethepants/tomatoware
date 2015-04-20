df378627c64d032587bbf6544013de2eb2bfd193
lancethepants <lancethepants@gmail.com>
2015-04-20 14:21:48 -0600

    Update git to latest. 2.3.5.

--- scripts/base.sh
+++ scripts/base.sh
@@ -1251,12 +1251,12 @@ fi
 cd $SRC/git
 
 if [ ! -f .extracted ]; then
-	rm -rf git-2.3.4
-	tar zxvf git-2.3.4.tar.gz
+	rm -rf git-2.3.5
+	tar zxvf git-2.3.5.tar.gz
 	touch .extracted
 fi
 
-cd git-2.3.4
+cd git-2.3.5
 
 if [ ! -f .built ]; then
 	make distclean
