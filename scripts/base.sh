0c2cd339e8c680ed035ae299eff20a46b8743b91
lancethepants <lancethepants@gmail.com>
2015-05-18 09:51:01 -0600

    Update libpcap to latest. 1.7.3.

--- scripts/base.sh
+++ scripts/base.sh
@@ -391,12 +391,12 @@ fi
 cd $SRC/libpcap
 
 if [ ! -f .extracted ]; then
-	rm -rf libpcap-1.7.2
-	tar zxvf libpcap-1.7.2.tar.gz
+	rm -rf libpcap-1.7.3
+	tar zxvf libpcap-1.7.3.tar.gz
 	touch .extracted
 fi
 
-cd libpcap-1.7.2
+cd libpcap-1.7.3
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
