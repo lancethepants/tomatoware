e4e068940f0aa476e7ced6a26c4b911696e928a5
lancethepants <lancethepants@gmail.com>
2015-05-18 18:22:11 -0600

    Make ARM compile as default.

--- config.mk
+++ config.mk
@@ -1,23 +1,24 @@
 # User Configurable Options
 
-export DESTARCH=mipsel
-#export DESTARCH=arm
+export DESTARCH=arm
+#export DESTARCH=mipsel
 
 export PREFIX=/mmc
 #export PREFIX=/jffs
-#export PREFIX=/opt/tomatoware
 #export PREFIX=/opt
+#export PREFIX=/opt/tomatoware
 
 # Do Not Edit
 
 export FLOAT=soft
+
+ifeq ($(DESTARCH), arm)
+export EXTRACFLAGS = -O2 -pipe -march=armv7-a -mtune=cortex-a9
+export PATH := $(PATH):/opt/tomatoware/$(DESTARCH)-$(FLOAT)$(subst /,-,$(PREFIX))/usr/bin/
+endif
 
 ifeq ($(DESTARCH), mipsel)
 export EXTRACFLAGS = -O2 -pipe -mips32 -mtune=mips32
 export PATH := $(PATH):/opt/tomatoware/$(DESTARCH)-$(FLOAT)$(subst /,-,$(PREFIX))/bin/
 endif
 
-ifeq ($(DESTARCH), arm)
-export EXTRACFLAGS = -O2 -pipe -march=armv7-a -mtune=cortex-a9
-export PATH := $(PATH):/opt/tomatoware/$(DESTARCH)-$(FLOAT)$(subst /,-,$(PREFIX))/usr/bin/
-endif
