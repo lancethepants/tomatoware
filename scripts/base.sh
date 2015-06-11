4bc8a531d32f7867ab5b222637931c7ab7b9b52c
lancethepants <lancethepants@gmail.com>
2015-06-11 08:17:04 -0600

    Update par2cmdline to latest. 0.6.13.

--- scripts/base.sh
+++ scripts/base.sh
@@ -1185,12 +1185,12 @@ fi
 cd $SRC/par2cmdline
 
 if [ ! -f .extracted ]; then
-	rm -rf par2cmdline-0.6.12
-	tar zxvf par2cmdline-0.6.12.tar.gz
+	rm -rf par2cmdline-0.6.13
+	tar zxvf par2cmdline-0.6.13.tar.gz
 	touch .extracted
 fi
 
-cd par2cmdline-0.6.12
+cd par2cmdline-0.6.13
 
 if [ ! -f .configured ]; then
 	aclocal
