2c450cb78398432d8a267b2b850e122cfffc9cc1
lancethepants <lancethepants@gmail.com>
2015-08-11 08:43:33 -0600

    Update openssh to latest. 7.0p1.

--- scripts/base.sh
+++ scripts/base.sh
@@ -1376,12 +1376,12 @@ fi
 cd $SRC/openssh
 
 if [ ! -f .extracted ]; then
-	rm -rf openssh-6.9p1
-	tar zxvf openssh-6.9p1.tar.gz
+	rm -rf openssh-7.0p1
+	tar zxvf openssh-7.0p1.tar.gz
 	touch .extracted
 fi
 
-cd openssh-6.9p1
+cd openssh-7.0p1
 
 if [ ! -f .patched ]; then
 	patch -p1 < $PATCHES/openssh/openssh-fix-pam-uclibc-pthreads-clash.patch
