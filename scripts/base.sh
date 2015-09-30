efe29c5b019b0e27871a6e3dea2c6335c2ffb91d
lancethepants <lancethepants@gmail.com>
2015-09-30 09:10:59 -0600

    Update xz utils to latest. 5.2.2.

--- scripts/base.sh
+++ scripts/base.sh
@@ -123,12 +123,12 @@ fi
 cd $SRC/xz
 
 if [ ! -f .extracted ]; then
-	rm -rf xz-5.2.1
-	tar zxvf xz-5.2.1.tar.gz
+	rm -rf xz-5.2.2
+	tar zxvf xz-5.2.2.tar.gz
 	touch .extracted
 fi
 
-cd xz-5.2.1
+cd xz-5.2.2
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
