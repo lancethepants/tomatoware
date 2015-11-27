febaa4be5edd52b27690f5994eaf3793579f23d1
lancethepants <lancethepants@gmail.com>
2015-11-27 11:17:27 -0700

    Update wget to latest. 1.17.

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -863,12 +863,12 @@ export PKG_CONFIG_LIBDIR=$DEST/lib/pkgconfig
 cd $SRC/wget
 
 if [ ! -f .extracted ]; then
-	rm -rf wget-1.16.3
-	tar zxvf wget-1.16.3.tar.gz
+	rm -rf wget-1.17
+	tar zxvf wget-1.17.tar.gz
 	touch .extracted
 fi
 
-cd wget-1.16.3
+cd wget-1.17
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
