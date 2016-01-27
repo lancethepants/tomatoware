59e84a3e5af69fbbceece5ca1dd1055dcb427254
lancethepants <lancethepants@gmail.com>
2016-01-27 09:24:06 -0700

    Update git to latest. 2.7.0.

--- scripts/base.sh
+++ scripts/base.sh
@@ -1259,12 +1259,12 @@ fi
 cd $SRC/git
 
 if [ ! -f .extracted ]; then
-	rm -rf git-2.6.4
-	tar zxvf git-2.6.4.tar.gz
+	rm -rf git-2.7.0
+	tar zxvf git-2.7.0.tar.gz
 	touch .extracted
 fi
 
-cd git-2.6.4
+cd git-2.7.0
 
 if [ ! -f .built ]; then
 	make distclean
