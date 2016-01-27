db7f2b1659204ef85baad633eaccee5b710d5226
lancethepants <lancethepants@gmail.com>
2016-01-27 09:18:15 -0700

    Update sqlite to latest. 3.10.2.

--- scripts/base.sh
+++ scripts/base.sh
@@ -659,12 +659,12 @@ fi
 cd $SRC/sqlite
 
 if [ ! -f .extracted ]; then
-	rm -rf sqlite-autoconf-3090200
-	tar zxvf sqlite-autoconf-3090200.tar.gz
+	rm -rf sqlite-autoconf-3100200
+	tar zxvf sqlite-autoconf-3100200.tar.gz
 	touch .extracted
 fi
 
-cd sqlite-autoconf-3090200
+cd sqlite-autoconf-3100200
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
