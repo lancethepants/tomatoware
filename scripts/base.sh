365ab6e6b4171a8a95364ca92e186c0faa1e03b7
lancethepants <lancethepants@gmail.com>
2016-03-09 07:55:55 -0700

    Update htop to latest. 2.0.1.

--- scripts/base.sh
+++ scripts/base.sh
@@ -1434,12 +1434,12 @@ fi
 cd $SRC/htop
 
 if [ ! -f .extracted ]; then
-	rm -rf htop-1.0.3
-	tar zxvf htop-1.0.3.tar.gz
+	rm -rf htop-2.0.1
+	tar zxvf htop-2.0.1.tar.gz
 	touch .extracted
 fi
 
-cd htop-1.0.3
+cd htop-2.0.1
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
