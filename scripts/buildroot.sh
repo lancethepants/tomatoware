3ca8b17e432ba9931c193709d7919d7e3d332f6d
lancethepants <lancethepants@gmail.com>
2016-03-24 09:06:42 -0600

    Update texinfo to latest. 6.1.

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -1002,12 +1002,12 @@ fi
 cd $SRC/texinfo
 
 if [ ! -f .extracted ]; then
-        rm -rf texinfo-6.0
-        tar zxvf texinfo-6.0.tar.gz
+        rm -rf texinfo-6.1
+        tar zxvf texinfo-6.1.tar.gz
         touch .extracted
 fi
 
-cd texinfo-6.0
+cd texinfo-6.1
 
 if [ ! -f .configured ]; then
         LDFLAGS=$LDFLAGS \
