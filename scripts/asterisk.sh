d00c2d890ac1a485bb08f95b3d585132bb3ed86d
lancethepants <lancethepants@gmail.com>
2016-04-21 11:46:45 -0600

    Update tzdata to latest. 2016d.

--- scripts/asterisk.sh
+++ scripts/asterisk.sh
@@ -347,8 +347,8 @@ cd $SRC/tz
 if [ ! -f .extracted ]; then
 	rm -rf tz tz-native
 	mkdir tz
-	tar zxvf tzcode2016c.tar.gz -C ./tz
-	tar zxvf tzdata2016c.tar.gz -C ./tz
+	tar zxvf tzcode2016d.tar.gz -C ./tz
+	tar zxvf tzdata2016d.tar.gz -C ./tz
 	cp -r tz tz-native
         touch .extracted
 fi
