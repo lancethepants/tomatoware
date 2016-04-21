e886a2d0dca978e0ee9c8811af65cc9848ee28c9
lancethepants <lancethepants@gmail.com>
2016-04-21 11:39:28 -0600

    Update gnuTLS to latest. 3.4.11.

--- scripts/asterisk.sh
+++ scripts/asterisk.sh
@@ -57,12 +57,12 @@ export PKG_CONFIG_LIBDIR=$DEST/lib/pkgconfig
 cd $SRC/gnutls
 
 if [ ! -f .extracted ]; then
-	rm -rf gnutls-3.4.10
-	tar xvJf gnutls-3.4.10.tar.xz
+	rm -rf gnutls-3.4.11
+	tar xvJf gnutls-3.4.11.tar.xz
 	touch .extracted
 fi
 
-cd gnutls-3.4.10
+cd gnutls-3.4.11
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
