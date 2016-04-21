ee947e324e7780f2304836f9d56af3642ee6f2e1
lancethepants <lancethepants@gmail.com>
2016-04-21 09:22:37 -0600

    Update cmake to latest. 3.5.2.

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -743,12 +743,12 @@ fi
 cd $SRC/cmake
 
 if [ ! -f .extracted ]; then
-	rm -rf cmake-3.5.0
-	tar zxvf cmake-3.5.0.tar.gz
+	rm -rf cmake-3.5.2
+	tar zxvf cmake-3.5.2.tar.gz
 	touch .extracted
 fi
 
-cd cmake-3.5.0
+cd cmake-3.5.2
 
 if [ ! -f .patched ]; then
 	patch -p1 < $PATCHES/cmake/cmake.patch
