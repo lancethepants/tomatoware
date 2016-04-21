034b8be0f772b981b415cc6aa0c5f034fca5986e
lancethepants <lancethepants@gmail.com>
2016-04-21 14:13:36 -0600

    Update golang to latest. 1.6.2.

--- src/golang/build_go.sh
+++ src/golang/build_go.sh
@@ -11,8 +11,8 @@ tar zxvf go1.4.3.src.tar.gz
 mv go go1.4 && cd ./go1.4/src
 ./make.bash
 cd $PREFIX/bin/go
-wget https://storage.googleapis.com/golang/go1.6.src.tar.gz
-tar zxvf go1.6.src.tar.gz -C $PREFIX/bin
+wget https://storage.googleapis.com/golang/go1.6.2.src.tar.gz
+tar zxvf go1.6.2.src.tar.gz -C $PREFIX/bin
 cd $PREFIX/bin/go/src
 wget -O $PREFIX/ssl/certs/ca-certificates.crt https://raw.githubusercontent.com/bagder/ca-bundle/master/ca-bundle.crt
 sed -i 's,\/etc\/ssl\/certs\/ca-certificates.crt,'"$PREFIX"'\/ssl\/certs\/ca-certificates.crt,g' \
