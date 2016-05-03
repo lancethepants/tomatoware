3de75e2f651e8121ea026cb72007a57d2dc299d4
lancethepants <lancethepants@gmail.com>
2016-05-03 08:14:26 -0600

    Update git to latest. 2.8.2.

--- scripts/base.sh
+++ scripts/base.sh
@@ -1267,12 +1267,12 @@ fi
 cd $SRC/git
 
 if [ ! -f .extracted ]; then
-	rm -rf git-2.8.1
-	tar zxvf git-2.8.1.tar.gz
+	rm -rf git-2.8.2
+	tar zxvf git-2.8.2.tar.gz
 	touch .extracted
 fi
 
-cd git-2.8.1
+cd git-2.8.2
 
 if [ ! -f .built ]; then
 	make distclean
