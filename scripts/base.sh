cf27a19681cc7dc24e3baf04d67a44adac64df7b
lancethepants <lancethepants@gmail.com>
2016-05-24 09:09:28 -0600

    Update gdbm to latest. 1.12.

--- scripts/base.sh
+++ scripts/base.sh
@@ -556,12 +556,12 @@ fi
 cd $SRC/libgdbm
 
 if [ ! -f .extracted ]; then
-	rm -rf gdbm-1.11
-	tar zxvf gdbm-1.11.tar.gz
+	rm -rf gdbm-1.12
+	tar zxvf gdbm-1.12.tar.gz
 	touch .extracted
 fi
 
-cd gdbm-1.11
+cd gdbm-1.12
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
