7efbcda3461f9833435da0037a29de01348ff7e6
lancethepants <lancethepants@gmail.com>
2016-05-24 09:24:08 -0600

    Update make to latest. 4.2.

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -710,12 +710,12 @@ fi
 cd $SRC/make
 
 if [ ! -f .extracted ]; then
-	rm -rf make-4.1
-	tar zxvf make-4.1.tar.gz
+	rm -rf make-4.2
+	tar zxvf make-4.2.tar.gz
 	touch .extracted
 fi
 
-cd make-4.1
+cd make-4.2
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
