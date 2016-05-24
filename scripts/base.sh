fbfb38cb71001748e3ddfd50a4f3cd9146bf049f
lancethepants <lancethepants@gmail.com>
2016-05-24 10:37:56 -0600

    Fix sqlite compilation.

--- scripts/base.sh
+++ scripts/base.sh
@@ -664,12 +664,12 @@ fi
 cd $SRC/sqlite
 
 if [ ! -f .extracted ]; then
-	rm -rf sqlite-autoconf-3130200
-	tar zxvf sqlite-autoconf-3130200.tar.gz
+	rm -rf sqlite-autoconf-3130000
+	tar zxvf sqlite-autoconf-3130000.tar.gz
 	touch .extracted
 fi
 
-cd sqlite-autoconf-3130200
+cd sqlite-autoconf-3130000
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
