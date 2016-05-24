83cc2c4d16a6bbd2210fa282084e9177eb49d061
lancethepants <lancethepants@gmail.com>
2016-05-24 09:15:53 -0600

    Update libxml2 to latest. 2.9.4.

--- scripts/base.sh
+++ scripts/base.sh
@@ -697,12 +697,12 @@ fi
 cd $SRC/libxml2
 
 if [ ! -f .extracted ]; then
-	rm -rf libxml2-2.9.3
-	tar zxvf libxml2-2.9.3.tar.gz
+	rm -rf libxml2-2.9.4
+	tar zxvf libxml2-2.9.4.tar.gz
 	touch .extracted
 fi
 
-cd libxml2-2.9.3
+cd libxml2-2.9.4
 
 if [ ! -f .configured ]; then
 	LDFLAGS="-lz -llzma $LDFLAGS" \
