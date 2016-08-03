65fdcb3b1a21024dd1ce5330443d9720533b3385
lancethepants <lancethepants@gmail.com>
2016-08-03 07:45:43 -0600

    Update asterisk to latest. 11.23.0.

--- scripts/asterisk.sh
+++ scripts/asterisk.sh
@@ -233,12 +233,12 @@ export PKG_CONFIG_LIBDIR=$DEST/lib/pkgconfig
 cd $SRC/asterisk
 
 if [ ! -f .extracted ]; then
-	rm -rf asterisk-11.22.0
-	tar zxvf asterisk-11.22.0.tar.gz
+	rm -rf asterisk-11.23.0
+	tar zxvf asterisk-11.23.0.tar.gz
 	touch .extracted
 fi
 
-cd asterisk-11.22.0
+cd asterisk-11.23.0
 
 if [ ! -f .patched ]; then
 	patch < $PATCHES/asterisk/010-asterisk-configure-undef-res-ninit.patch
