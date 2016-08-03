dba4781731401a4c7ae4e578f8635d16a5f4cdba
lancethepants <lancethepants@gmail.com>
2016-08-03 07:05:42 -0600

    Update cmake to altest. 3.6.1.

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -751,12 +751,12 @@ fi
 cd $SRC/cmake
 
 if [ ! -f .extracted ]; then
-	rm -rf cmake-3.5.2
-	tar zxvf cmake-3.5.2.tar.gz
+	rm -rf cmake-3.6.1
+	tar zxvf cmake-3.6.1.tar.gz
 	touch .extracted
 fi
 
-cd cmake-3.5.2
+cd cmake-3.6.1
 
 if [ ! -f .patched ]; then
 	patch -p1 < $PATCHES/cmake/cmake.patch
