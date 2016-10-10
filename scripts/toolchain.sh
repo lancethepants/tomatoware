ff55e757ad6cc5f840bd9999b2aab782bd74e486
lancethepants <lancethepants@gmail.com>
2016-10-10 07:20:08 -0600

    uclibc-ng: Patch to fix static linking.

--- scripts/toolchain.sh
+++ scripts/toolchain.sh
@@ -22,7 +22,7 @@ then
 	mv $BASE/toolchain/patches/linux-headers.$DESTARCH $BASE/toolchain/patches/linux-headers
 	echo "UCLIBC_HAS_BACKTRACE=y" >> $BASE/toolchain/buildroot-2016.05/package/uclibc/uClibc-ng.config
 	echo "# UCLIBC_USE_MIPS_PREFETCH is not set" >> $BASE/toolchain/buildroot-2016.05/package/uclibc/uClibc-ng.config
-	echo "sha256 46d6c73393625671e5152e81e65595a2a7f30479d20f3527a5bb9e1b5452a287 uClibc-ng-1.0.18.tar.xz" >> $BASE/toolchain/buildroot-2016.05/package/uclibc/uclibc.hash
+	echo "sha256 c7fd3e17bcfb8c3fd6963d16dbf9e3ce025c987fbfca38a1fe5a99c711eb1377 uClibc-ng-1.0.18.tar.xz" >> $BASE/toolchain/buildroot-2016.05/package/uclibc/uclibc.hash
 	sed -i 's,1.0.14,1.0.18,g' $BASE/toolchain/buildroot-2016.05/package/uclibc/uclibc.mk
 
 	sed -i 's,\/opt,'"$PREFIX"',g' \
