b87ac2e1a5e94af25b3c6bf6e2601bd04755f8dd
lancethepants <lancethepants@gmail.com>
2016-11-01 07:46:37 -0600

    Update git to latest. 2.10.2.

--- scripts/base.sh
+++ scripts/base.sh
@@ -1278,12 +1278,12 @@ fi
 cd $SRC/git
 
 if [ ! -f .extracted ]; then
-	rm -rf git-2.10.0
-	tar zxvf git-2.10.0.tar.gz
+	rm -rf git-2.10.2
+	tar zxvf git-2.10.2.tar.gz
 	touch .extracted
 fi
 
-cd git-2.10.0
+cd git-2.10.2
 
 if [ ! -f .built ]; then
 	make distclean
