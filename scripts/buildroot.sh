8b66aa82eac0c1191c17884801f263193220b8b4
lancethepants <lancethepants@gmail.com>
2016-12-07 14:57:26 -0700

    Update grep to latest. 2.27.

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -929,12 +929,12 @@ unset PKG_CONFIG_LIBDIR
 cd $SRC/grep
 
 if [ ! -f .extracted ]; then
-	rm -rf grep-2.26
-	tar xvJf grep-2.26.tar.xz
+	rm -rf grep-2.27
+	tar xvJf grep-2.27.tar.xz
 	touch .extracted
 fi
  
-cd grep-2.26
+cd grep-2.27
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
