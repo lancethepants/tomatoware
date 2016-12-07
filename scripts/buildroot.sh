2be03cf920104882f27ec71c6dfa55bb8aea26a8
lancethepants <lancethepants@gmail.com>
2016-12-07 13:28:13 -0700

    Update util-linux to latest. 2.29.

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -815,12 +815,12 @@ fi
 cd $SRC/util-linux
 
 if [ ! -f .extracted ]; then
-	rm -rf util-linux-2.28
-	tar zxvf util-linux-2.28.tar.gz
+	rm -rf util-linux-2.29
+	tar zxvf util-linux-2.29.tar.gz
 	touch .extracted
 fi
 
-cd util-linux-2.28
+cd util-linux-2.29
 
 if [ ! -f .patched ] && [ "$DESTARCH" == "mipsel" ];then
 	sed -i 's,epoll_create1,epoll_create,g' ./libmount/src/monitor.c
