8575bb4b1be4a74bf0861552263aaa0b13bf6255
lancethepants <lancethepants@gmail.com>
2016-12-28 08:55:29 -0700

    Update uclibc-ng to latest. 1.0.21.

--- scripts/toolchain.sh
+++ scripts/toolchain.sh
@@ -22,9 +22,9 @@ then
 	mv $BASE/toolchain/patches/linux-headers.$DESTARCH $BASE/toolchain/patches/linux-headers
 	echo "UCLIBC_HAS_BACKTRACE=y" >> $BASE/toolchain/buildroot-2016.11/package/uclibc/uClibc-ng.config
 	echo "# UCLIBC_USE_MIPS_PREFETCH is not set" >> $BASE/toolchain/buildroot-2016.11/package/uclibc/uClibc-ng.config
-#	echo "sha256 1632cac9ec87875e888dea75a38354c45e419c17559099b6e3880641746a5c0f uClibc-ng-1.0.19.tar.xz" >> $BASE/toolchain/buildroot-2016.11/package/uclibc/uclibc.hash
-#	sed -i 's,1.0.14,1.0.19,g' $BASE/toolchain/buildroot-2016.11/package/uclibc/uclibc.mk
-	sed -i 's,1c817672a65cf9132c98f84e1b8445650de1c18eca258f49c0050b420a25e946,80a5ac500dd859da9e868fdf9ef540aab9e375af42401a0fb0e39b03c79ae8c2,g' $BASE/toolchain/buildroot-2016.11/package/uclibc/uclibc.hash
+	echo "sha256 6e99ebebe8a46ddcd26615828d51b62dbf580568aa58bf5f495da37eba0c872d uClibc-ng-1.0.21.tar.xz" >> $BASE/toolchain/buildroot-2016.11/package/uclibc/uclibc.hash
+	sed -i 's,1.0.20,1.0.21,g' $BASE/toolchain/buildroot-2016.11/package/uclibc/uclibc.mk
+#	sed -i 's,1c817672a65cf9132c98f84e1b8445650de1c18eca258f49c0050b420a25e946,80a5ac500dd859da9e868fdf9ef540aab9e375af42401a0fb0e39b03c79ae8c2,g' $BASE/toolchain/buildroot-2016.11/package/uclibc/uclibc.hash
 
 	sed -i 's,\/opt,'"$PREFIX"',g' \
 	$BASE/toolchain/patches/uclibc/001-uclibc-ldso-search-path.patch \
