a5c866a7b12982d3b912c65f6add4d09b1ae9b42
lancethepants <lancethepants@gmail.com>
2017-02-16 11:26:24 -0700

    Update screen to latest. 4.5.0.

--- scripts/base.sh
+++ scripts/base.sh
@@ -1486,12 +1486,12 @@ fi
 cd $SRC/screen
 
 if [ ! -f .extracted ]; then
-	rm -rf screen-4.4.0
-	tar zxvf screen-4.4.0.tar.gz
+	rm -rf screen-4.5.0
+	tar zxvf screen-4.5.0.tar.gz
 	touch .extracted
 fi
 
-cd screen-4.4.0
+cd screen-4.5.0
 
 if [ ! -f .patched ]; then
 	patch < $PATCHES/screen/screen.patch
