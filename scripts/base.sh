d9e7757b0341682259f15f6a65c423446c2fff66
lancethepants <lancethepants@gmail.com>
2017-02-22 22:07:13 -0700

    Revert "Update strace to latest. 4.16." Wait til they fix things on mips.
    This reverts commit ab7014d49500944d0708e9b9278bda98c5c26a0c.

--- scripts/base.sh
+++ scripts/base.sh
@@ -1328,12 +1328,12 @@ fi
 cd $SRC/strace
 
 if [ ! -f .extracted ]; then
-	rm -rf strace-4.16
-	tar xvJf strace-4.16.tar.xz
+	rm -rf strace-4.14
+	tar xvJf strace-4.14.tar.xz
 	touch .extracted
 fi
 
-cd strace-4.16
+cd strace-4.14
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
