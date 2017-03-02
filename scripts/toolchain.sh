8357cb719a77705b90b29f507de20dcbf0d03137
lancethepants <lancethepants@gmail.com>
2017-03-02 15:23:12 -0700

    Update toolchain. use GCC-6.3.

--- scripts/toolchain.sh
+++ scripts/toolchain.sh
@@ -16,15 +16,15 @@ fi
 if [ ! -d /opt/tomatoware/$DESTARCH-$FLOAT${PREFIX////-} ]
 then
 	mkdir $BASE/toolchain
-	tar zxvf $SRC/toolchain/buildroot-2016.11.tar.gz -C $BASE/toolchain
-	cp $SRC/toolchain/defconfig.$DESTARCH $BASE/toolchain/buildroot-2016.11/defconfig
+	tar zxvf $SRC/toolchain/buildroot-2017.02.tar.gz -C $BASE/toolchain
+	cp $SRC/toolchain/defconfig.$DESTARCH $BASE/toolchain/buildroot-2017.02/defconfig
 	cp -r $SRC/toolchain/patches $BASE/toolchain
 	mv $BASE/toolchain/patches/linux-headers.$DESTARCH $BASE/toolchain/patches/linux-headers
-	echo "UCLIBC_HAS_BACKTRACE=y" >> $BASE/toolchain/buildroot-2016.11/package/uclibc/uClibc-ng.config
-	echo "# UCLIBC_USE_MIPS_PREFETCH is not set" >> $BASE/toolchain/buildroot-2016.11/package/uclibc/uClibc-ng.config
-	echo "sha256 1c7e5cf82b034929edb9c7e584abfaf1c62fb087b64b73fcfc6315570690119a uClibc-ng-1.0.22.tar.xz" >> $BASE/toolchain/buildroot-2016.11/package/uclibc/uclibc.hash
-	sed -i 's,1.0.20,1.0.22,g' $BASE/toolchain/buildroot-2016.11/package/uclibc/uclibc.mk
-#	sed -i 's,1c817672a65cf9132c98f84e1b8445650de1c18eca258f49c0050b420a25e946,80a5ac500dd859da9e868fdf9ef540aab9e375af42401a0fb0e39b03c79ae8c2,g' $BASE/toolchain/buildroot-2016.11/package/uclibc/uclibc.hash
+	echo "UCLIBC_HAS_BACKTRACE=y" >> $BASE/toolchain/buildroot-2017.02/package/uclibc/uClibc-ng.config
+	echo "# UCLIBC_USE_MIPS_PREFETCH is not set" >> $BASE/toolchain/buildroot-2017.02/package/uclibc/uClibc-ng.config
+#	echo "sha256 1c7e5cf82b034929edb9c7e584abfaf1c62fb087b64b73fcfc6315570690119a uClibc-ng-1.0.22.tar.xz" >> $BASE/toolchain/buildroot-2017.02/package/uclibc/uclibc.hash
+#	sed -i 's,1.0.20,1.0.22,g' $BASE/toolchain/buildroot-2017.02/package/uclibc/uclibc.mk
+	sed -i 's,f2004c85db8e07e9f1c2e8b7c513fa7c237bc9f9685d8e1bfc89535b8a85449b,1c7e5cf82b034929edb9c7e584abfaf1c62fb087b64b73fcfc6315570690119a,g' $BASE/toolchain/buildroot-2017.02/package/uclibc/uclibc.hash
 
 	sed -i 's,\/opt,'"$PREFIX"',g' \
 	$BASE/toolchain/patches/uclibc/001-uclibc-ldso-search-path.patch \
@@ -32,7 +32,7 @@ then
 	$BASE/toolchain/patches/uclibc/003-uclibc-dl-defs.patch \
 	$BASE/toolchain/patches/uclibc/004-uclibc-ldd-opt.patch
 
-	cd $BASE/toolchain/buildroot-2016.11
+	cd $BASE/toolchain/buildroot-2017.02
 	make defconfig BR2_DEFCONFIG=defconfig
 	make
 
