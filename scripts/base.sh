02ce443f9b8e825232b1699f37246a5c026b356b
lancethepants <lancethepants@gmail.com>
2017-09-05 11:31:34 -0600

    Update libxml2 to latest. 2.9.5.

--- scripts/base.sh
+++ scripts/base.sh
@@ -699,12 +699,12 @@ fi
 cd $SRC/libxml2
 
 if [ ! -f .extracted ]; then
-	rm -rf libxml2-2.9.4
-	tar zxvf libxml2-2.9.4.tar.gz
+	rm -rf libxml2-2.9.5
+	tar zxvf libxml2-2.9.5.tar.gz
 	touch .extracted
 fi
 
-cd libxml2-2.9.4
+cd libxml2-2.9.5
 
 if [ ! -f .configured ]; then
 	Z_CFLAGS=-I$DEST/include \
