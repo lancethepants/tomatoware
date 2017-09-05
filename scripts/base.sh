9b4763a50af4a1125bb20f92d41ca628259e3b68
lancethepants <lancethepants@gmail.com>
2017-09-05 11:11:22 -0600

    Update lzo to latest. 2.10.

--- scripts/base.sh
+++ scripts/base.sh
@@ -89,12 +89,12 @@ fi
 cd $SRC/lzo
 
 if [ ! -f .extracted ]; then
-	rm -rf lzo-2.09
-	tar zxvf lzo-2.09.tar.gz
+	rm -rf lzo-2.10
+	tar zxvf lzo-2.10.tar.gz
 	touch .extracted
 fi
 
-cd lzo-2.09
+cd lzo-2.10
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
