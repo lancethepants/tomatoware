a2c62b2f62a57e005ecb5262d95f4bc2de0aac62
lancethepants <lancethepants@gmail.com>
2017-09-05 11:18:19 -0600

    Update curl to latest. 7.55.1.

--- scripts/base.sh
+++ scripts/base.sh
@@ -316,12 +316,12 @@ fi
 cd $SRC/curl
 
 if [ ! -f .extracted ]; then
-	rm -rf curl-7.52.1
-	tar zxvf curl-7.52.1.tar.gz
+	rm -rf curl-7.55.1
+	tar zxvf curl-7.55.1.tar.gz
 	touch .extracted
 fi
 
-cd curl-7.52.1
+cd curl-7.55.1
 
 if [ ! -f .configured ]; then
 	PKG_CONFIG_PATH="$DEST/lib/pkgconfig" \
@@ -350,7 +350,7 @@ if [ ! -f .certs_installed ]; then
 	cd $DEST/ssl/certs
 	curl https://curl.haxx.se/ca/cacert.pem | awk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "cert" n ".pem"}'
 	c_rehash .
-	touch $SRC/curl/curl-7.52.1/.certs_installed
+	touch $SRC/curl/curl-7.55.1/.certs_installed
 fi
 
 ######### ###################################################################
