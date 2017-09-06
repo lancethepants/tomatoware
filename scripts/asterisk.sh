a3a156070848041189899e7bc09f768d7c6f2232
lancethepants <lancethepants@gmail.com>
2017-09-06 10:56:25 -0600

    Update gnutls to latest. 3.6.0.

--- scripts/asterisk.sh
+++ scripts/asterisk.sh
@@ -57,12 +57,12 @@ export PKG_CONFIG_LIBDIR=$DEST/lib/pkgconfig
 cd $SRC/gnutls
 
 if [ ! -f .extracted ]; then
-	rm -rf gnutls-3.5.9
-	tar xvJf gnutls-3.5.9.tar.xz
+	rm -rf gnutls-3.6.0
+	tar xvJf gnutls-3.6.0.tar.xz
 	touch .extracted
 fi
 
-cd gnutls-3.5.9
+cd gnutls-3.6.0
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
