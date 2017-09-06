b200e0d324fe6b1e854849c09e2293259cd6b188
lancethepants <lancethepants@gmail.com>
2017-09-06 08:38:52 -0600

    Update binutils to latest. 2.29.

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -222,8 +222,8 @@ fi
 cd $SRC/binutils
 
 if [ ! -f .extracted ]; then
-	rm -rf binutils-2.27 build-binutils
-	tar zxvf binutils-2.27.tar.gz
+	rm -rf binutils-2.29 build-binutils
+	tar zxvf binutils-2.29.tar.gz
 	mkdir build-binutils
 	touch .extracted
 fi
@@ -243,7 +243,7 @@ if [ ! -f .configured ]; then
 	CPPFLAGS=$CPPFLAGS \
 	CFLAGS=$CFLAGS \
 	CXXFLAGS=$CXXFLAGS \
-	../binutils-2.27/configure --prefix=$PREFIX --host=$os --target=$os \
+	../binutils-2.29/configure --prefix=$PREFIX --host=$os --target=$os \
 	--with-sysroot=$PREFIX \
 	--disable-werror \
 	--disable-nls
