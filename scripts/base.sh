f215a473b283cd76b3c608b02f53f4d190cfdf7a
lancethepants <lancethepants@gmail.com>
2017-11-21 08:26:24 -0700

    Update strace to latest. 4.20.

--- scripts/base.sh
+++ scripts/base.sh
@@ -1328,12 +1328,12 @@ fi
 cd $SRC/strace
 
 if [ ! -f .extracted ]; then
-	rm -rf strace-4.14
-	tar xvJf strace-4.14.tar.xz
+	rm -rf strace-4.20
+	tar xvJf strace-4.20.tar.xz
 	touch .extracted
 fi
 
-cd strace-4.14
+cd strace-4.20
 
 if [ ! -f .configured ]; then
 	LDFLAGS=$LDFLAGS \
