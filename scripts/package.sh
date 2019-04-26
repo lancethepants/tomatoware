3d70182e8ee1643ba7bb5e49f8cce44dd5e039dc
lancethepants <lancethepants@gmail.com>
2019-04-26 12:03:43 -0600

    Fix pkg library path for a couple .pc files.

--- scripts/package.sh
+++ scripts/package.sh
@@ -56,6 +56,10 @@ find $DEST/lib -iname \*.la -exec sed -i 's,'"$BASE"',,g' {} \;
 #Change the base library libtool (.la) files to reference their correct location in the target system.
 find $DEST/lib -iname \*.la -exec sed -i 's,\/opt\/tomatoware\/'"$DESTARCH"'-'"$FLOAT"''"${PREFIX////-}"'\/'"$DESTARCH"'-linux-uclibc,'"$PREFIX"',g' {} \;
 
+#Remove build path directory $BASE from all pkg-config .pc files.
+#This makes sure the pkg-config .pc files show the correct paths to libraries for the deployment system.
+find $DEST/lib/pkgconfig -iname \*.pc -exec sed -i 's,'"$DEST"','"$PREFIX"',g' {} \;
+
 #Make sure all perl scripts have the correct interpreter path.
 grep -Irl "\#\!\/usr\/bin\/perl" $DEST | xargs sed -i -e '1,1s,\#\!\/usr\/bin\/perl,\#\!'"$PREFIX"'\/bin\/perl,g'
 grep -Irl "\#\! \/usr\/bin\/perl" $DEST | xargs sed -i -e '1,1s,\#\! \/usr\/bin\/perl,\#\! '"$PREFIX"'\/bin\/perl,g'
