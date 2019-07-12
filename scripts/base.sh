64069b6e02b9fdd38ebc6940262e58d18836fbe3
lancethepants <lancethepants@gmail.com>
2019-07-12 10:47:54 -0600

    Fix boost compilation.

--- scripts/base.sh
+++ scripts/base.sh
@@ -1862,6 +1862,11 @@ fi
 
 cd boost_${BOOST_VERSION}
 
+if [ ! -f .patched ]; then
+        patch -p1 < $PATCHES/boost/boost.patch
+        touch .patched
+fi
+
 if ! [[ -f .configured ]]; then
 	echo  "using gcc : $DESTARCH : $DESTARCH-linux-g++ ;" > $SRC/boost/user-config.jam
 	./bootstrap.sh
