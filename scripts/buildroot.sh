ec62985ea63fd3e467aaad8804772df0294625f9
lancethepants <lancethepants@gmail.com>
2020-02-27 13:26:21 -0700

    Revert upx to 3.94 for now.

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -1703,7 +1703,7 @@ unset PYTHON_CROSS
 ####### #####################################################################
 
 UCL_VERSION=1.03
-UPX_VERSION=3.96
+UPX_VERSION=3.94
 
 export UPX_UCLDIR=$SRC/upx/ucl-${UCL_VERSION}
 
