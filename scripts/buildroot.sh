0c2f08ae584d2baceac973f80e5fa0e38ba7b234
lancethepants <lancethepants@gmail.com>
2022-03-07 11:24:46 -0700

    Update meson to latest. 0.61.2.

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -7,7 +7,7 @@ source ./scripts/environment.sh
 ######### ###################################################################
 Status "meson"
 
-MESON_VERSION=0.61.1
+MESON_VERSION=0.61.2
 
 cd $SRC/meson
 
