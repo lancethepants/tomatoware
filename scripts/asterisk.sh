e5f5fb83508a911089c02ce7445977e11d7e7102
lancethepants <lancethepants@gmail.com>
2022-06-08 08:21:16 -0600

    nettle: update to version 3.8.

--- scripts/asterisk.sh
+++ scripts/asterisk.sh
@@ -7,7 +7,7 @@ source ./scripts/environment.sh
 ########## ##################################################################
 Status "nettle"
 
-NETTLE_VERSION=3.7.3
+NETTLE_VERSION=3.8
 
 cd $SRC/nettle
 
