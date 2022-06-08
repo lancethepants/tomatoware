35da45c342f2c06215ef4944620acd59a619c7b7
lancethepants <lancethepants@gmail.com>
2022-06-08 08:25:38 -0600

    apt: update to version 2.5.0.

--- scripts/asterisk.sh
+++ scripts/asterisk.sh
@@ -757,7 +757,7 @@ fi
 ####### #####################################################################
 Status "apt"
 
-APT_VERSION=2.4.5
+APT_VERSION=2.5.0
 
 export PKG_CONFIG_LIBDIR=$DEST/lib/pkgconfig
 
