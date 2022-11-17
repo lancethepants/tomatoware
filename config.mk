a5d78b9b2faee84fa2183438c1866b505a42aa67
lancethepants <lancethepants@gmail.com>
2022-11-17 08:35:38 -0700

    config.mk: Remove trailing slash in PATH definition.

--- config.mk
+++ config.mk
@@ -30,24 +30,24 @@ export BUILDCROSSTOOLS ?= 1
 ifeq ($(DESTARCH), mipsel)
 export DESTARCHLIBC = uclibc
 export EXTRACFLAGS = -O2 -pipe -mips32 -mtune=mips32
-export PATH := $(PATH):/opt/tomatoware/mipsel-$(DESTARCHLIBC)$(subst /,-,$(PREFIX))/bin/
+export PATH := $(PATH):/opt/tomatoware/mipsel-$(DESTARCHLIBC)$(subst /,-,$(PREFIX))/bin
 endif
 
 ifeq ($(DESTARCH), arm)
 export DESTARCHLIBC = $(ARM_LIBC)
 export EXTRACFLAGS = -O2 -pipe -march=armv7-a -mtune=cortex-a9
-export PATH := $(PATH):/opt/tomatoware/arm-$(DESTARCHLIBC)$(subst /,-,$(PREFIX))/bin/
-export PATH := $(PATH):/opt/tomatoware/mipsel-uclibc$(subst /,-,$(PREFIX))/bin/
+export PATH := $(PATH):/opt/tomatoware/arm-$(DESTARCHLIBC)$(subst /,-,$(PREFIX))/bin
+export PATH := $(PATH):/opt/tomatoware/mipsel-uclibc$(subst /,-,$(PREFIX))/bin
 endif
 
 ifeq ($(DESTARCH), aarch64)
 export DESTARCHLIBC = musl
 export EXTRACFLAGS = -mcpu=cortex-a53
-export PATH := $(PATH):/opt/tomatoware/aarch64-$(DESTARCHLIBC)$(subst /,-,$(PREFIX))/bin/
+export PATH := $(PATH):/opt/tomatoware/aarch64-$(DESTARCHLIBC)$(subst /,-,$(PREFIX))/bin
 endif
 
 ifeq ($(DESTARCH), x86_64)
 export DESTARCHLIBC = musl
 export EXTRACFLAGS =
-export PATH := $(PATH):/opt/tomatoware/x86_64-$(DESTARCHLIBC)$(subst /,-,$(PREFIX))/bin/
+export PATH := $(PATH):/opt/tomatoware/x86_64-$(DESTARCHLIBC)$(subst /,-,$(PREFIX))/bin
 endif
