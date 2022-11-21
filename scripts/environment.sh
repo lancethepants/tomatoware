7f34c10f055e2da2aa3de5ae0b17aeeac5aaeffc
lancethepants <lancethepants@gmail.com>
2022-11-21 07:31:20 -0700

    scripts/environment.sh: Fix things in the right order.

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -7,46 +7,39 @@ BASE=`pwd`
 SRC=$BASE/src
 PATCHES=$BASE/patches
 DEST=$BASE$PREFIX
-
-if [ "$DESTARCHLIBC" == "uclibc" ]; then
-	LDFLAGS="-L$DEST/lib -s -Wl,--dynamic-linker=$PREFIX/lib/ld-uClibc.so.1 -Wl,-rpath-link,$DEST/lib"
-fi
-
-if [ "$DESTARCHLIBC" == "musl" ]; then
-	LDFLAGS="-L$DEST/lib -s -Wl,--dynamic-linker=$PREFIX/lib/ld-musl-$DESTARCH.so.1 -Wl,-rpath-link,$DEST/lib"
-fi
-
 CPPFLAGS="-I$DEST/include"
 CFLAGS=$EXTRACFLAGS
 CXXFLAGS=$CFLAGS
-CONFIGURE="./configure --prefix=$PREFIX --host=$DESTARCH-tomatoware-linux-$DESTARCHLIBC$EABI"
+
 MAKE1="make"
 MAKE="make -j`nproc`"
 NINJA="ninja"
+
 ORIGINALPATH=$PATH
 export PATH=$BASE/native/bin:$PATH
+
 export CCACHE_DIR=$HOME/.ccache
 export CCACHE_NOINODECACHE=1
 
-if [ "$DESTARCH" = "arm" ]; then
-	if [ "$DESTARCHLIBC" = "uclibc" ]; then
+if [ "$DESTARCHLIBC" == "uclibc" ]; then
+	LDFLAGS="-L$DEST/lib -s -Wl,--dynamic-linker=$PREFIX/lib/ld-uClibc.so.1 -Wl,-rpath-link,$DEST/lib"
+	if [ "$DESTARCH" = "arm" ]; then
 		EABI=gnueabi
 		if [ "$BUILDCROSSTOOLS" == "1" ]; then
 			MIPSEL=mipsel-tomatoware-linux-uclibc
 		fi
 	fi
-	if [ "$DESTARCHLIBC" = "musl" ]; then
+fi
+
+if [ "$DESTARCHLIBC" == "musl" ]; then
+	LDFLAGS="-L$DEST/lib -s -Wl,--dynamic-linker=$PREFIX/lib/ld-musl-$DESTARCH.so.1 -Wl,-rpath-link,$DEST/lib"
+	if [ "$DESTARCH" = "arm" ]; then
 		EABI=eabi
 	fi
 fi
 
+CONFIGURE="./configure --prefix=$PREFIX --host=$DESTARCH-tomatoware-linux-$DESTARCHLIBC$EABI"
 export LT_SYS_LIBRARY_PATH="$PREFIX/lib $DEST/lib /opt/tomatoware/$DESTARCH-$DESTARCHLIBC${PREFIX////-}/$DESTARCH-tomatoware-linux-$DESTARCHLIBC$EABI/lib"
-
-if [ "$DESTARCH" == "aarch64" ]; then
-	PERL_VERSION=5.32.1
-else
-	PERL_VERSION=5.36.0
-fi
 
 BINUTILS_VERSION=2.38
 GCC_VERSION=12.2.0
@@ -54,6 +47,12 @@ NINJA_VERSION=1.11.1
 CMAKE_VERSION=3.24.3
 CCACHE_VERSION=4.7.3
 AUTOCONF_VERSION=2.71
+
+if [ "$DESTARCH" == "aarch64" ]; then
+	PERL_VERSION=5.32.1
+else
+	PERL_VERSION=5.36.0
+fi
 
 if [ "$BUILDLLVM" == "1" ] && [ "$DESTARCH" == "arm" ]; then
 	GCCforClang="5.1.0"
